9aa66b47da2ff698b67d5e0313cac844b344167c
Abhijeet <ajpalanki@gmail.com>
2019-11-27 14:14:16 -0600

    environment keys

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  PIXABAY_API_KEY: "8249294-65beeb9a7fa7d25d73d51c4bb",
+  PIXABAY_API_URL: "https://pixabay.com/api/?key="
 };
